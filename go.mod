eb3bb2a229de23593060daa8f8067ea7a5b68c9b
MerjaR <merja.rossi@gmail.com>
2025-06-09 23:23:55 +0300

    Adding main.go and initial files

---
+++ go.mod
@@ -0,0 +1,8 @@
+module forum
+
+go 1.22.1
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.23
+	golang.org/x/crypto v0.27.0
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"forum/internal/db"
+	"forum/internal/handlers"
+)
+
+func main() {
+	// Initialize the SQLite database
+	db.InitDB("forum.db")
+	db.RunMigrations()
+
+	// Create a new multiplexer (router)
+	mux := http.NewServeMux()
+
+	// Handle static files
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
+
+	// Home page
+	mux.HandleFunc("/", handlers.HomeHandler)
+
+	// Posts
+	mux.HandleFunc("/api/posts", handlers.PostsHandler)
+	mux.HandleFunc("/create-post", handlers.CreatePostHandler)
+	mux.HandleFunc("/delete-post", handlers.DeletePostHandler)
+	mux.HandleFunc("/edit-post", handlers.EditPostHandler)
+
+	// Authentication
+	mux.HandleFunc("/register", handlers.RegisterHandler)
+	mux.HandleFunc("/login", handlers.LoginHandler)
+	mux.HandleFunc("/logout", handlers.LogoutHandler)
+
+	// User
+	mux.HandleFunc("/accept-cookies", handlers.AcceptCookiesHandler)
+	mux.HandleFunc("/profile", handlers.ProfileHandler)
+	mux.HandleFunc("/upload-profile-picture", handlers.UploadProfilePictureHandler)
+
+	//Comments and reactions
+	mux.HandleFunc("/add-comment", handlers.AddCommentHandler)
+	mux.HandleFunc("/react", handlers.ReactionHandler)
+	mux.HandleFunc("/react-comment", handlers.CommentReactionHandler)
+
+	//Filtering and search
+	mux.HandleFunc("/filter", handlers.FilterHandler)
+	mux.HandleFunc("/search", handlers.SearchHandler)
+
+	// Start the server on port 8080
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
